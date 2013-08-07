54e47a51d543bbcdfcabf4c0b306669ee7714e4c
Matteo Moci <matteo.moci@gmail.com>
2013-08-07 02:13:08 +0200

    upgraded rx to 0.10.0

--- src/test/java/org/mox/spikes/rx/AppTestCase.java
+++ src/test/java/org/mox/spikes/rx/AppTestCase.java
@@ -29,7 +29,7 @@ public class AppTestCase {
             }
 
             @Override
-            public void onError(Exception e) {
+            public void onError(Throwable throwable) {
                 LOGGER.info("error");
             }
 
