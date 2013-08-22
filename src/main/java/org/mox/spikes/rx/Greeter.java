1f88abf2f3f37e6913eab57da97e99d54d59af8d
Matteo Moci <matteo.moci@gmail.com>
2013-08-22 13:33:47 +0200

    changed test logic

--- src/main/java/org/mox/spikes/rx/Greeter.java
+++ src/main/java/org/mox/spikes/rx/Greeter.java
@@ -8,36 +8,42 @@ import rx.Observer;
 import rx.Subscription;
 import rx.util.functions.Func1;
 
-public class App {
+public class Greeter {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(Greeter.class);
 
     public static Observable<String> hello(String... names) {
 
-        return Observable.create(new Func1<Observer<String>, Subscription>() {
+        Func1<Observer<String>, Subscription> func = new Func1<Observer<String>, Subscription>() {
 
             @Override
             public Subscription call(final Observer<String> stringObserver) {
+
                 final Thread t = new Thread(new Runnable() {
                     @Override
                     public void run() {
+
                         for (int i = 0; i < 75; i++) {
 
                             stringObserver.onNext("a_value-" + i);
+
                         }
                         stringObserver.onCompleted();
                     }
                 });
+
                 t.start();
 
                 return new Subscription() {
                     @Override
                     public void unsubscribe() {
+
                         t.interrupt();
                     }
                 };
             }
-        });
+        };
+        return Observable.create(func);
     }
 
 }
