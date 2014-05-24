97571676227b080855d924729b6c1878ee9fad84
Matteo Moci <matteo.moci@gmail.com>
2014-05-24 21:10:53 +0200

    more tests

--- src/test/java/org/mox/spikes/rx/RxTestCase.java
+++ src/test/java/org/mox/spikes/rx/RxTestCase.java
@@ -5,15 +5,40 @@ import rx.Observable;
 import rx.functions.Func1;
 import rx.observables.BlockingObservable;
 
+import java.util.Iterator;
+
 import static org.testng.Assert.assertEquals;
 
 /**
  * @author Matteo Moci ( matteo (dot) moci (at) gmail (dot) com )
  */
-public class StreamsTestCase {
+public class RxTestCase {
 
     @Test
-    public void testName() throws Exception {
+    public void shouldUseTestScheduler() throws Exception {
+
+        final Observable<Integer> twoItems = Observable.from(1, 2);
+        final Observable<Integer> doubler = twoItems.map(
+                new Func1<Integer, Integer>() {
+
+                    @Override
+                    public Integer call(Integer integer) {
+
+                        return integer * 2;
+                    }
+                }
+        );
+
+        final BlockingObservable<Integer> integerBlockingObservable = doubler
+                .toBlockingObservable();
+
+        final Iterator<Integer> iterator = integerBlockingObservable.getIterator();
+        assertEquals(iterator.next(), Integer.valueOf(2));
+        assertEquals(iterator.next(), Integer.valueOf(4));
+    }
+
+    @Test
+    public void shouldCalculateLength() throws Exception {
 
         final Observable<String> aName = Observable.just("this");
         final Observable<Integer> length = length(aName);
