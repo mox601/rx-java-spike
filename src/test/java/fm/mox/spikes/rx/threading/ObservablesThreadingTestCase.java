ba9d00337966dbe54fd5ab078047fd82ef9aee47
Matteo Moci <mmoci@expedia.com>
2018-05-16 15:26:16 +0200

    upgrade to java 8

--- src/test/java/fm/mox/spikes/rx/threading/ObservablesThreadingTestCase.java
+++ src/test/java/fm/mox/spikes/rx/threading/ObservablesThreadingTestCase.java
@@ -3,10 +3,10 @@ package fm.mox.spikes.rx.threading;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
+
 import rx.Observable;
 import rx.Subscriber;
 import rx.Subscription;
-import rx.functions.Action1;
 import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 
@@ -16,24 +16,29 @@ import rx.schedulers.Schedulers;
 public class ObservablesThreadingTestCase {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(
-            ObservablesThreadingTestCase.class);
+        ObservablesThreadingTestCase.class);
 
     @Test(enabled = true)
     public void testName() throws Exception {
 
         //functions
-        final Func1<Long, Long> oneAdder = new FastOp();
-        final Func1<Long, Long> doubler = new SlowOp();
+        final Func1<Long, Long> oneAdder = aLong -> {
+            LOGGER.info("fast: " + aLong);
+            return aLong;
+        };
+        final Func1<Long, Long> doubler = aLong -> {
+            try {
+                Thread.sleep(1000L);
+            } catch (InterruptedException e) {
+                //nop
+            }
+            LOGGER.info("slow: " + aLong);
+            return aLong;
+        };
 
         //aSource works on Schedulers.computation()
-        final Observable<Long> aSource = Observable.just(1L, 2L, 3L, 4L, 5L).doOnNext(
-                new Action1<Long>() {
-                    @Override
-                    public void call(final Long aLong) {
-
-                        LOGGER.info("tick: " + aLong);
-                    }
-                });
+        final Observable<Long> aSource = Observable.just(1L, 2L, 3L, 4L, 5L)
+            .doOnNext(aLong -> LOGGER.info("tick: " + aLong));
 
         //still computation()
         final Observable<Long> plusOne = aSource.map(oneAdder);
@@ -65,55 +70,18 @@ public class ObservablesThreadingTestCase {
 
         //runs on computation()
         final Observable<Long> afterLogging = doublePlusOneTick.observeOn(Schedulers.computation())
-                .map(new Func1<Long, Long>() {
-                    @Override
-                    public Long call(Long aLong) {
-
-                        LOGGER.info("on computation " + aLong);
-                        return aLong;
-                    }
-                });
+            .map(aLong -> {
+                LOGGER.info("on computation " + aLong);
+                return aLong;
+            });
 
         //runs on computation()
-        final Subscription anotherSubscription = afterLogging.subscribe(new Action1<Long>() {
-            @Override
-            public void call(Long aLong) {
-
-                LOGGER.info("on computation " + aLong);
-            }
-        });
+        final Subscription anotherSubscription = afterLogging
+            .subscribe(aLong -> LOGGER.info("on computation " + aLong));
 
         Thread.sleep(10000L);
 
         aSubscription.unsubscribe();
         anotherSubscription.unsubscribe();
-
-    }
-
-    private static class SlowOp implements Func1<Long, Long> {
-
-        @Override
-        public Long call(final Long aLong) {
-
-            try {
-                Thread.sleep(1000L);
-            } catch (InterruptedException e) {
-                //nop
-            }
-
-            LOGGER.info("slow: " + aLong);
-            return aLong;
-        }
-
-    }
-
-    private static class FastOp implements Func1<Long, Long> {
-
-        @Override
-        public Long call(final Long item) {
-
-            LOGGER.info("fast: " + item);
-            return item;
-        }
     }
 }
